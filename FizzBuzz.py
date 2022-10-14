21843c06fcf689db184ffa789e9538f19ee17a47
Ty Weatherly <77644015+tweatherly8@users.noreply.github.com>
2022-10-14 16:16:17 -0500

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,8 +1,12 @@
+#print numbers 1-100
 for num in range(1,101):
+    #print FizzBuzz for multiples of 3 & 5
     if num % 15 is 0:
         print("FizzBuzz")
+    #print Buzz for multiples of 5
     elif num % 5 is 0:
         print("Buzz")
+    #print Fizz for multiples of 3
     elif num % 3 is 0:
         print("Fizz")
     else:
